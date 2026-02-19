4e15caf3b270cc856a62674fa4a731ea1e7b6cd2
TheEternalice <wsl@DESKTOP-2G2EKDB.localdomain>
2026-02-19 14:33:31 +0100

    fix(server.ts): now should work with nginx and https

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,34 @@
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
 import { connectDatabase } from './config/database';
 import User from "./models/User";
 import userRoutes from "./routes/index";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 app.use("/users", userRoutes);
+
 async function bootstrap() {
 	await connectDatabase();
 	// Création des tables si elles n'existent pas
 	await User.sync();
 	console.log("✅ User table synced");
-	app.listen(PORT, () => {
-		console.log(`Server is running on https://app:${PORT}`);
+
+	const httpsOptions = {
+		key: fs.readFileSync('/etc/nginx/certs/server.key'),
+		cert: fs.readFileSync('/etc/nginx/certs/server.crt'),
+	};
+
+	https.createServer(httpsOptions, app).listen(PORT, () => {
+		console.log("HTTPS Server running on port ${PORT}");
 	});
+	// app.listen(PORT, () => {
+	// 	console.log(`Server is running on https://app:${PORT}`);
+	// });
 }
 
 bootstrap();
